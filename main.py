6be4f4e364f2d4f908295d6b0223f499e3c2f88e
Daniel Araújo <daniel.araujo@buser.com.br>
2024-09-12 13:22:25 -0300

    feat: add Nossas Senhoras: Fatima, Guadalupe and Carmo

--- main.py
+++ main.py
@@ -19,8 +19,11 @@ import os
 app = FastAPI()
 
 PDF_MAP = {
+    'nsa_sra_aparecida.pdf': 'Nossa Senhora Aparecida',
     'nsa_sra_gracas.pdf': 'Nossa Senhora das Graças',
-    'nsa_sra_aparecida.pdf': 'Nossa Senhora Aparecida'
+    'nsa_sra_fatima': 'Nossa Senhora de Fátima',
+    'nsa_sra_guadalupe': 'Nossa Senhora de Guadalupe',
+    'nsa_sra_carmo': 'Nossa Senhora do Carmo'
 }
 
 
